6e3408e863a299d5c870e7b60dfbf23223f961e3
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-04 01:57:08 +0530

    Added table to category , food and order menus

--- Admin/manage-food.php
+++ Admin/manage-food.php
@@ -1,5 +1,47 @@
 <?php include("Partials/menu.php") ?>
 <div class="wrapper">
-    <h1>Food Dashboard</h1>
+    <h1>Manage Food</h1>
+    <br><br>
+    <a href="#" class="btn-primary">Add food</a>
+
+    <br><br><br>
+    <table class="tbl-full">
+        <tr>
+            <th>S.N.</th>
+            <th>Full Name</th>
+            <th>UserName</th>
+            <th>Actions</th>
+        </tr>
+        <tr>
+            <td>1.</td>
+            <td>Astha Borah</td>
+            <td>asthaborah1</td>
+            <td>
+                <a href="#" class="btn-success">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+            </td>
+
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>Pranay Jain</td>
+            <td>PranayJain09</td>
+            <td>
+                <a href="#" class="btn-success">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+            </td>
+
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>Prachi shirsale</td>
+            <td>shirsalePrachi1</td>
+            <td>
+                <a href="#" class="btn-success">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+            </td>
+
+        </tr>
+    </table>
 </div>
 <?php include("Partials/Footer.php") ?>
