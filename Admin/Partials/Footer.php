1cc7126453084b54d32a90e58f947ba5f0df9af7
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-03 22:32:14 +0530

    Made a partials folder and added footer and menu for reusability

---
+++ Admin/Partials/Footer.php
@@ -0,0 +1,10 @@
+
+<!-- Footer starts here -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="footer-text text-align">2023 All rights reserved , Some restaurent . Developed by : <a href="#">Astha
+                Borah</a></p>
+    </div>
+</div>
+
+<!-- Footer ends here -->
---
+++ Admin/Partials/menu.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <title>Food Order Website</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<!-- menu section starts here -->
+<div class="menu">
+    <div class="wrapper text-align">
+        <ul>
+            <a href="index.php">
+                <li>Home</li>
+            </a>
+            <a href="Manage-admin.php">
+                <li>Admin</li>
+            </a>
+            <a href="#">
+                <li>Category</li>
+            </a>
+            <a href="#">
+                <li>Food</li>
+            </a>
+            <a href="#">
+                <li>Order</li>
+            </a>
+        </ul>
+    </div>
+</div>
+<!-- menu section ends here -->
+
+<html>
