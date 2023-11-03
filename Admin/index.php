fef7e7851e8166dc66142de63238faae9ff0b53e
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-03 19:57:06 +0530

    Designed admin page in index.php folder

---
+++ Admin/index.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+    <title>Food Order Website</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<!-- Body of the html -->
+
+<body>
+    <div class="menu">
+        <div class="wrapper text-align">
+            <ul>
+                <a href = "#"><li>Home</li></a>
+                <a href = "#"><li>Admin</li></a>
+                <a href = "#"><li>Category</li></a>
+                <a href = "#"><li>Food</li></a>
+                <a href = "#"><li>Order</li></a>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard</h1>
+            <div class="col-4 text-align">
+                <h3>5</h3>
+                Categories
+            </div>
+            <div class="col-4 text-align">
+                <h3>5</h3>
+                Categories
+            </div>
+            <div class="col-4 text-align">
+                <h3>5</h3>
+                Categories
+            </div>
+            <div class="col-4 text-align">
+                <h3>5</h3>
+                Categories
+            </div>
+
+            <div class = "clearfix"></div>
+        </div>
+
+    </div>
+    <div class="footer">
+        <div class="wrapper">
+            <p class = "footer-text text-align">2023 All rights reserved , Some restaurent . Developed by : <a href = "#">Astha Borah</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
