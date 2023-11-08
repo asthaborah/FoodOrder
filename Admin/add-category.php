d08010ad40565000b476e855b3678937306cbc25
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-08 19:49:26 +0530

    Fixed the bug for not adding category when image is not added

--- Admin/add-category.php
+++ Admin/add-category.php
@@ -5,15 +5,15 @@
         <h1>Add category</h1>
         <br>
 
-        <?php 
+        <?php
         //if the category is failed to get added
-            if(isset($_SESSION["add"])) {
-                echo $_SESSION["add"];
-                unset($_SESSION["add"]);
-            }
+        if (isset($_SESSION["add"])) {
+            echo $_SESSION["add"];
+            unset($_SESSION["add"]);
+        }
 
         //if the image is failed to upload
-        if(isset($_SESSION["upload"])) {
+        if (isset($_SESSION["upload"])) {
             echo $_SESSION["upload"];
             unset($_SESSION["upload"]);
         }
@@ -60,7 +60,7 @@
 
                 <tr>
                     <td colspan="2">
-                        <input type="submit" name="submit" value="Add category" class = "btn-success">
+                        <input type="submit" name="submit" value="Add category" class="btn-success">
                     </td>
                 </tr>
             </table>
@@ -68,98 +68,104 @@
     </div>
     <!-- Form for category ends here -->
 
-    <?php 
-        //check if the button is pressed or not
-        if(isset($_POST['submit'])){
-            //get the data from the form
-            $title = $_POST['title'];
+    <?php
+    //check if the button is pressed or not
+    if (isset($_POST['submit'])) {
+        //get the data from the form
+        $title = $_POST['title'];
 
-            //check whether the image is selected or not 
-            // print_r($_FILES['image']);
-            // die();
-            //how image is selected , Array ( [name] => maggi masala.jpg [type] => image/jpeg [tmp_name] => C:\xampp\tmp\phpADEF.tmp [error] => 0 [size] => 140922 )
+        //check whether the image is selected or not 
+        // print_r($_FILES['image']);
+        // die();
+        //how image is selected , Array ( [name] => maggi masala.jpg [type] => image/jpeg [tmp_name] => C:\xampp\tmp\phpADEF.tmp [error] => 0 [size] => 140922 )
+    
+        if (isset($_FILES['image']['name'])) { //This method only check if button is pressed or not there are changes where user go for upload but cancels it
+            //it will check if the image is selected or not with image name
+            //now check if image is uploaded or not
+            $image_name = $_FILES['image']['name'];
 
-            if(isset($_FILES['image']['name'])){
-                //it will check if the image is selected or not with image name
-                //now check if image is uploaded or not
-                $image_name = $_FILES['image']['name'];
-
+            //check whether the image is uploaded or not
+            if ($image_name != "") {
                 //we don't want if the image has same name then it will overwrite the image to prevent this we will randomly assign value
                 $ext = explode('.', $image_name);
                 $extension = end($ext);
 
                 //set the random value
-                $image_name = "Food_category" . rand(000 , 999) . '.' . $extension;
+                $image_name = "Food_category" . rand(000, 999) . '.' . $extension;
 
                 //to upload image we need image name , destination path and source path
                 $source_path = $_FILES['image']['tmp_name'];
 
                 $destination_path = "../images/Category/" . $image_name; //destination where image will get uploaded in local machine
-
+    
                 $upload = move_uploaded_file($source_path, $destination_path);
                 //check if image is uploaded or not
-
+    
                 //if the image upload is successfull it will get uploaded to your files where you have set the path
-                if($upload == false){
+                if ($upload == false) {
                     //if the image is not uploaded 
                     //set the error session
-
+    
                     $_SESSION['upload'] = "<div class = 'error'>image not uploaded</div>";
 
                     //Redirect to the add-category page again and end the process so that data don't get uploaded in the database
-
+    
                     header("location:" . SITEURL . "admin/add-category.php");
 
                     die();
                 }
 
             }else{
-                //didn't upload images that's why set the image name as blank
                 $image_name = "";
             }
 
+        } else {
+            //didn't upload images that's why set the image name as blank
+            $image_name = "";
+        }
 
-            if(isset($_POST['featured'])){
-                //if the radio button is pressed
-                $featured = $_POST['featured'];
-            }else{
-                //set the featured as default
-                $featured = "No";
-            }
 
-            if(isset($_POST['active'])){
-                //if the radio button is pressed
-                $active = $_POST['active'];
-            }else{
-                //set the featured as default
-                $active = "No";
-            }
+        if (isset($_POST['featured'])) {
+            //if the radio button is pressed
+            $featured = $_POST['featured'];
+        } else {
+            //set the featured as default
+            $featured = "No";
+        }
 
-            //create the sql query
-            $sql = "INSERT INTO tbl_category SET
+        if (isset($_POST['active'])) {
+            //if the radio button is pressed
+            $active = $_POST['active'];
+        } else {
+            //set the featured as default
+            $active = "No";
+        }
+
+        //create the sql query
+        $sql = "INSERT INTO tbl_category SET
             title = '$title',
             image_name = '$image_name',
             featured = '$featured',
             active = '$active'";
 
-            //execute the query
-            $res = mysqli_query($conn , $sql);
+        //execute the query
+        $res = mysqli_query($conn, $sql);
 
-            //check if the query is executed or not
-            if($res){
-                //query is executed
-                $_SESSION["add"] = "<div class = 'success'>Category added successfully</div>";
+        //check if the query is executed or not
+        if ($res) {
+            //query is executed
+            $_SESSION["add"] = "<div class = 'success'>Category added successfully</div>";
 
-                //redirect to the manage-category page
-                header("location:" . SITEURL . "admin/manage-category.php");
-            }else{
-                //query is executed
-                $_SESSION["add"] = "<div class = 'error'>Failed to add category</div>";
+            //redirect to the manage-category page
+            header("location:" . SITEURL . "admin/manage-category.php");
+        } else {
+            //query is executed
+            $_SESSION["add"] = "<div class = 'error'>Failed to add category</div>";
 
-                //redirect to the manage-category page
-                header("location:" . SITEURL . "admin/add-category.php");
-            }
+            //redirect to the manage-category page
+            header("location:" . SITEURL . "admin/add-category.php");
         }
+    }
     ?>
 </div>
 
