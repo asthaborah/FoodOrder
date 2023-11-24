c525e834a829e908df13ceac2e049227703fd466
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-24 21:47:21 +0530

    Added access control for update page

--- Admin/update-food.php
+++ Admin/update-food.php
@@ -1,3 +1,9 @@
 <?php include("Partials/menu.php"); ?>
-
+<?php 
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }else{
+        header("location:" . SITEURL . "admin/manage-food.php");
+    }
+?>
 <?php include("Partials/Footer.php"); ?>
