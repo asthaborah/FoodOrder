0479505f62a1de8037599d1a5ce0eba875789975
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-11-24 21:37:26 +0530

    Added url for update button

--- Admin/manage-food.php
+++ Admin/manage-food.php
@@ -70,7 +70,7 @@
                             <td><?php echo $featured; ?></td>
                             <td><?php echo $active; ?></td>
                             <td>
-                                <a href="#" class="btn-success">Update Food</a>
+                                <a href="<?php echo SITEURL?>admin/update-food.php?id=<?php echo $id?>" class="btn-success">Update Food</a>
                                 <a href="<?php echo SITEURL?>admin/delete-food.php?id=<?php echo $id?>&image_name=<?php echo $image_name;?>" class="btn-danger">Delete Food</a>
                             </td>
 
