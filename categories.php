ad6bd621bfbfba53f08d71bcadc83c78d98c08d4
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-12-28 16:55:42 +0530

    Added all the active category in the category section through backend

--- categories.php
+++ categories.php
@@ -1,28 +1,62 @@
 <?php include("Partials-front/menu.php"); ?>
 
-    <!-- CAtegories Section Starts Here -->
-    <section class="categories">
-        <div class="container">
-            <h2 class="text-center">Explore Foods</h2>
+<!-- CAtegories Section Starts Here -->
+<section class="categories">
+    <div class="container">
+        <h2 class="text-center">Explore Foods</h2>
 
-            <!-- code for fetching categories from the backend -->
-            <?php
-            
-                //query for displaying the categories
-                $sql = "SELECT * FROM tbl_category WHERE active = 'Yes";
-            ?>
+        <!-- code for fetching categories from the backend -->
+        <?php
 
-            <a href="category-foods.html">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+        //query for displaying the categories
+        $sql = "SELECT * FROM tbl_category WHERE active = 'Yes'";
+        //executing the query
+        $res = mysqli_query($conn, $sql);
 
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
-            </a>    
+        //count the rows
+        $count = mysqli_num_rows($res);
 
-            <div class="clearfix"></div>
-        </div>
-    </section>
-    <!-- Categories Section Ends Here -->
+        //check according to count if the categories exist or not
+        if ($count > 0) {
+            //categories exist
+            //loop to fetch all the categories
+            while ($row = mysqli_fetch_assoc($res)) {
+                //fetch the id , title and image_name
+                $id = $row['id'];
+                $title = $row['title'];
+                $image_name = $row['image_name'];
+
+                ?>
+                <a href="category-foods.html">
+                    <div class="box-3 float-container">
+                        <?php 
+                            //check if the image exist or not
+                            if($image_name == ""){
+                                //image doesn't exist
+                                echo "<div class = 'error'>Image not added</div>";
+                            }else{
+                                //image exist
+                                ?>
+                                <img src="<?php echo SITEURL;?>images/Category/<?php echo $image_name?>" alt="Pizza" class="img-responsive img-curve">
+                                <?php
+                            }
+                        ?>
+
+                        <h3 class="float-text text-white"><?php echo $title ?></h3>
+                    </div>
+                </a>
+                <?php
+            }
+        } else {
+            //category doesn't exist
+            echo "<div class = 'error'>Category not added</div>";
+        }
+
+        ?>
+
+        <div class="clearfix"></div>
+    </div>
+</section>
+<!-- Categories Section Ends Here -->
 
 <?php include("Partials-front/footer.php"); ?>
