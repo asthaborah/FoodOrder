fd4f001f252bc6b429eb58bf24100690e7554c76
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-12-30 23:23:00 +0530

    fetched data for food items from the database

--- foods.php
+++ foods.php
@@ -1,75 +1,82 @@
 <?php include("Partials-front/menu.php"); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <form action="food-search.html" method="POST">
-                <input type="search" name="search" placeholder="Search for Food.." required>
-                <input type="submit" name="submit" value="Search" class="btn btn-primary">
-            </form>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+        <form action="food-search.html" method="POST">
+            <input type="search" name="search" placeholder="Search for Food.." required>
+            <input type="submit" name="submit" value="Search" class="btn btn-primary">
+        </form>
+
+    </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
 
 
 
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+<!-- fOOD MEnu Section Starts Here -->
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Food Menu</h2>
 
-            <!-- Adding functionality for fetching food data from the database -->
-            <?php
-                $sql = "SELECT * FROM tbl_food WHERE active = 'Yes'";
-                //executing the query
+        <!-- Adding functionality for fetching food data from the database -->
+        <?php
+        $sql = "SELECT * FROM tbl_food WHERE active = 'Yes'";
+        //executing the query
+        
+        $res = mysqli_query($conn, $sql);
 
-                $res = mysqli_query($conn , $sql);
+        //count the rows
+        
+        $count = mysqli_num_rows($res);
 
-                //count the rows
+        if ($count > 0) {
+            //food exist
+            while ($row = mysqli_fetch_assoc($res)) {
+                $title = $row['title'];
+                $description = $row['description'];
+                $image_name = $row['image_name'];
+                $price = $row['price'];
 
-                $count = mysqli_num_rows($res);
+                ?>
+                <div class="food-menu-box">
+                    <div class="food-menu-img">
+                        <?php 
+                            if($image_name == ""){
+                                echo "<div class = 'error'>Image not added</div>";
+                            }else{
+                                ?>
+                                <img src="<?php echo SITEURL;?>images/Food/<?php echo $image_name;?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                                <?php
+                            }
+                        ?>
+                    </div>
 
-                if($count > 0){
-                    //food exist
-                    while($row = mysqli_fetch_assoc($res)){
-                        $id = $row['id'];
-                        $title = $row['title'];
-                        $description = $row['description'];
-                        $image_name = $row['image_name'];
-                        $price = $row['price'];
-                    }
-                }else{
-                    //food doesn't exist
-                    echo "<div class = 'error'>Food not added</div>";
-                }
-            ?>
+                    <div class="food-menu-desc">
+                        <h4><?php echo $title; ?></h4>
+                        <p class="food-price">₹<?php echo $price; ?></p>
+                        <p class="food-detail"><?php echo $description; ?></p>
+                        <br>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                        <a href="#" class="btn btn-primary">Order Now</a>
+                    </div>
                 </div>
+                <?php
+            }
+        } else {
+            //food doesn't exist
+            echo "<div class = 'error'>Food not added</div>";
+        }
+        ?>
 
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
+        <div class="clearfix"></div>
 
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
 
-            <div class="clearfix"></div>
 
-            
+    </div>
 
-        </div>
-
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
+</section>
+<!-- fOOD Menu Section Ends Here -->
 
 <?php include("Partials-front/footer.php"); ?>
