88f62dfae6c341e1adb39424cb10bdb06c79eab3
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-12-30 23:23:50 +0530

    Added rupees symbol

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
 
                     <div class="food-menu-desc">
                         <h4><?php echo $title;?></h4>
-                        <p class="food-price"><?php echo $price;?></p>
+                        <p class="food-price">₹<?php echo $price;?></p>
                         <p class="food-detail"><?php echo $description;?></p>
                         <br>
                         <a href="order.html" class="btn btn-primary">Order Now</a>
