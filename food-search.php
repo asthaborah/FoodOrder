f53b7087a7a2d7f98f6e6306cc99ae28eeae24f1
asthaborah <89447681+asthaborah@users.noreply.github.com>
2023-12-31 15:52:21 +0530

    deleted other food items which were showing static data

--- food-search.php
+++ food-search.php
@@ -1,160 +1,75 @@
 <?php include("Partials-front/menu.php"); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <h2>Foods on Your Search <a href="#" class="text-white">"Momo"</a></h2>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+        <h2>Foods on Your Search <a href="#" class="text-white">"Momo"</a></h2>
+
+    </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
 
 
 
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+<!-- fOOD MEnu Section Starts Here -->
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Food Menu</h2>
 
-            <!-- implementing search through database -->
-            <?php
-                //getting keyword for search
-                $search = $_POST['search'];
+        <!-- implementing search through database -->
+        <?php
+        //getting keyword for search
+        $search = $_POST['search'];
 
-                //sql for searching an item
-                $sql = "SELECT * FROM tbl_food WHERE title LIKE %$search% OR description LIKE %$search%";
+        //sql for searching an item
+        $sql = "SELECT * FROM tbl_food WHERE title LIKE %$search% OR description LIKE %$search%";
 
-                //executing the query
-                $res = mysqli_query($conn , $sql);
+        //executing the query
+        $res = mysqli_query($conn, $sql);
 
-                //count the rows
-                $count = mysqli_num_rows($res);
+        //count the rows
+        $count = mysqli_num_rows($res);
 
-                if($count > 0){
-                    //food exist
-                    while($row == mysqli_fetch_assoc($res)){
-                        $title = $row['title'];
-                        $description = $row['description'];
-                        $image_name = $row['image_name'];
-                        $price = $row['price'];
-                    }
-                }else{
-                    //food doesn't exist
-                    echo "<div class = 'error'>Food doesn't exist</div>";
-                }
-            ?>
+        if ($count > 0) {
+            //food exist
+            while ($row == mysqli_fetch_assoc($res)) {
+                $title = $row['title'];
+                $description = $row['description'];
+                $image_name = $row['image_name'];
+                $price = $row['price'];
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                ?>
+                <div class="food-menu-box">
+                    <div class="food-menu-img">
+                        <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                    </div>
+
+                    <div class="food-menu-desc">
+                        <h4>Food Title</h4>
+                        <p class="food-price">$2.3</p>
+                        <p class="food-detail">
+                            Made with Italian Sauce, Chicken, and organice vegetables.
+                        </p>
+                        <br>
+
+                        <a href="#" class="btn btn-primary">Order Now</a>
+                    </div>
                 </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Smoky Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Nice Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-momo.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Chicken Steam Momo</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
+                <?php
+            }
+        } else {
+            //food doesn't exist
+            echo "<div class = 'error'>Food doesn't exist</div>";
+        }
+        ?>
+        <div class="clearfix"></div>
 
 
-            <div class="clearfix"></div>
 
-            
+    </div>
 
-        </div>
-
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
+</section>
+<!-- fOOD Menu Section Ends Here -->
 
 <?php include("Partials-front/footer.php"); ?>
