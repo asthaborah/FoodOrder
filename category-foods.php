6ec8469f92f55c6886082dff8ac8c0aeb468c5ee
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-01-01 23:55:05 +0530

    Added functionality for showing food according to categories

--- category-foods.php
+++ category-foods.php
@@ -1,78 +1,91 @@
 <?php include("Partials-front/menu.php"); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <h2>Foods on <a href="#" class="text-white">"Category"</a></h2>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+        <h2>Foods on <a href="#" class="text-white">"Category"</a></h2>
 
-    <!-- category id received through get method -->
-    <?php 
-        if(isset($_GET['category_id'])){
-            //security measure so that no one can access without clicking on category
-            $category_id = $_GET['category_id'];
-        }else{
-            header("location:" . SITEURL . "index.php");
-        }
-    ?>
+    </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
+
+<!-- category id received through get method -->
+<?php
+if (isset($_GET['category_id'])) {
+    //security measure so that no one can access without clicking on category
+    $category_id = $_GET['category_id'];
+} else {
+    header("location:" . SITEURL . "index.php");
+}
+?>
 
 
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+<!-- fOOD MEnu Section Starts Here -->
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Food Menu</h2>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-                <!-- query for displaying foods based on category -->
-                <?php 
-                    $sql = "SELECT * FROM tbl_food WHERE category_id = $category_id";
+        <!-- query for displaying foods based on category -->
+        <?php
+        $sql = "SELECT * FROM tbl_food WHERE category_id = $category_id";
 
-                    //executing the query
+        //executing the query
+        
+        $res = mysqli_query($conn, $sql);
 
-                    $res = mysqli_query($conn , $sql);
+        //count the rows
+        $count = mysqli_num_rows($res);
 
-                    //count the rows
-                    $count = mysqli_num_rows($res);
-
-                    if($count > 0){
-                        // food exist
-                        while($row = mysqli_fetch_assoc($res)){
-                            $id = $row['id']; 
-                            $title = $row['title']; 
-                            $description = $row['description']; 
-                            $price = $row['price']; 
-                            $image_name = $row['image_name']; 
-                        }
-                    }else{
-                        //food doesn't exist
-                        echo "<div class = 'error'>Food doesn't exist</div>";
-                    }
+        if ($count > 0) {
+            // food exist
+            while ($row = mysqli_fetch_assoc($res)) {
+                $id = $row['id'];
+                $title = $row['title'];
+                $description = $row['description'];
+                $price = $row['price'];
+                $image_name = $row['image_name'];
                 ?>
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
+                <div class="food-menu-box">
+                    <div class="food-menu-img">
+                        <?php 
+                            if($image_name == ""){
+                                echo "<div class = 'error'>Image not added</div>";
+                            }else{
+                                ?>
+                                <img src="<?php echo SITEURL;?>images/Food/<?php echo $image_name;?>" alt="Food" class="img-responsive img-curve">
+                                <?php
+                            }
+                        ?>
+                    </div>
+                    <div class="food-menu-desc">
+                        <h4>
+                            <?php echo $title; ?>
+                        </h4>
+                        <p class="food-price">$
+                            <?php echo $price; ?>
+                        </p>
+                        <p class="food-detail">
+                            <?php echo $description; ?>
+                        </p>
+                        <br>
 
-                    <a href="#" class="btn btn-primary">Order Now</a>
+                        <a href="#" class="btn btn-primary">Order Now</a>
+                    </div>
                 </div>
-            </div>
-            <div class="clearfix"></div>
 
-            
+                <?php
+            }
+        } else {
+            //food doesn't exist
+            echo "<div class = 'error'>Food doesn't exist</div>";
+        }
+        ?>
+        <div class="clearfix"></div>
 
-        </div>
+    </div>
 
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
+</section>
+<!-- fOOD Menu Section Ends Here -->
 
 <?php include("Partials-front/footer.php"); ?>
