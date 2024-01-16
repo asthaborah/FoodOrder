01621ddc4e4251794143c126d18b6cd61bac7657
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-01-16 23:29:33 +0530

    Added css and changed size to improve the structure of the table

--- Admin/manage-order.php
+++ Admin/manage-order.php
@@ -1,92 +1,114 @@
 <?php include("Partials/menu.php") ?>
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Manage Order</h1>
+<html>
 
-        <br><br><br>
-        <table class="tbl-full">
-            <!-- displaying all the details of the order in the table -->
-            <tr>
-                <th>S.N.</th>
-                <th>Food</th>
-                <th>Price</th>
-                <th>Qty</th>
-                <th>Total</th>
-                <th>Order Date</th>
-                <th>Status</th>
-                <th>Customer Name</th>
-                <th>Contact</th>
-                <th>Email</th>
-                <th>Address</th>
-                <th>Actions</th>
-            </tr>
-            <?php
-            $sql = "SELECT * FROM tbl_order";
-            $res = mysqli_query($conn, $sql);
-            if ($res) {
-                $sn = 1;
-                while ($row = mysqli_fetch_assoc($res)) {
-                    $food = $row['food'];
-                    $price = $row['price'];
-                    $qty = $row['qty'];
-                    $total = $row['total'];
-                    $order_date = $row['order_date'];
-                    $status = $row['status'];
-                    $customer_name = $row['customer_name'];
-                    $customer_contact = $row['customer_contact'];
-                    $customer_email = $row['customer_email'];
-                    $customer_address = $row['customer_address'];
+<head>
+    <style>
+        td{
+            font-size: 12px;
+            border-collapse: collapse;
+            text-align: left;
+        }
 
-                    ?>
-                    <tr>
-                        <td>
-                            <?php echo $sn++ ?>
-                        </td>
-                        <td>
-                            <?php echo $food ?>
-                        </td>
-                        <td>
-                            <?php echo $price ?>
-                        </td>
-                        <td>
-                            <?php echo $qty ?>
-                        </td>
-                        <td>
-                            <?php echo $total ?>
-                        </td>
-                        <td>
-                            <?php echo $order_date ?>
-                        </td>
-                        <td>
-                            <?php echo $status ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_name ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_contact ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_email ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_address ?>
-                        </td>
-                        <td>
-                            <a href="#" class="btn-success">Update Admin</a>
-                        </td>
+        .td-wrap{
+            word-wrap: break-word;
+        }
+    </style>
+</head>
+    <div class="main-content">
+        <div style="width:95%" class="wrapper">
+            <h1>Manage Order</h1>
 
-                    </tr>
+            <br><br><br>
+            <table class="tbl-full">
+                <!-- displaying all the details of the order in the table -->
+                <tr>
+                    <th>S.N.</th>
+                    <th>Food</th>
+                    <th>Price</th>
+                    <th>Qty</th>
+                    <th>Total</th>
+                    <th>Order Date</th>
+                    <th>Status</th>
+                    <th>Customer Name</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Actions</th>
+                </tr>
+                <?php
+                $sql = "SELECT * FROM tbl_order";
+                $res = mysqli_query($conn, $sql);
+                if ($res) {
+                    $count = mysqli_num_rows($res);
+                    if ($count > 0) {
+                        $sn = 1;
+                        while ($row = mysqli_fetch_assoc($res)) {
+                            $food = $row['food'];
+                            $price = $row['price'];
+                            $qty = $row['qty'];
+                            $total = $row['total'];
+                            $order_date = $row['order_date'];
+                            $status = $row['status'];
+                            $customer_name = $row['customer_name'];
+                            $customer_contact = $row['customer_contact'];
+                            $customer_email = $row['customer_email'];
+                            $customer_address = $row['customer_address'];
 
-                    <?php
+                            ?>
+                            <tr>
+                                <td>
+                                    <?php echo $sn++ ?>
+                                </td>
+                                <td class = "td-wrap" colspan="1">
+                                    <?php echo $food ?>
+                                </td>
+                                <td>
+                                    <?php echo $price ?>
+                                </td>
+                                <td>
+                                    <?php echo $qty ?>
+                                </td>
+                                <td>
+                                    <?php echo $total ?>
+                                </td>
+                                <td>
+                                    <?php echo $order_date ?>
+                                </td>
+                                <td>
+                                    <?php echo $status ?>
+                                </td>
+                                <td>
+                                    <?php echo $customer_name ?>
+                                </td>
+                                <td>
+                                    <?php echo $customer_contact ?>
+                                </td>
+                                <td>
+                                    <?php echo $customer_email ?>
+                                </td>
+                                <td class = "td-wrap">
+                                    <?php echo $customer_address ?>
+                                </td>
+                                <td>
+                                    <a href="#" class="btn-success">Update Admin</a>
+                                </td>
 
+                            </tr>
+
+                            <?php
+
+                        }
+                    } else {
+                        echo "<tr><td colspan = '12' class = 'error'>Orders not available</td></tr>";
+                    }
                 }
-            } else {
 
-            }
-            ?>
+                ?>
 
-        </table>
+            </table>
+        </div>
     </div>
-</div>
+
+</html>
+
 <?php include("Partials/Footer.php") ?>
