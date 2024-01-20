09c3c55b6ea3116864f92fffe09b68308edccf68
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-01-20 21:54:17 +0530

    Implemented dashboard

--- Admin/index.php
+++ Admin/index.php
@@ -5,28 +5,86 @@
     <div class="wrapper">
         <h1>Dashboard</h1>
 
-        <?php 
-            if(isset($_SESSION['login'])){
-                echo $_SESSION['login'];
-                unset($_SESSION['login']);
-            }
+        <?php
+        if (isset($_SESSION['login'])) {
+            echo $_SESSION['login'];
+            unset($_SESSION['login']);
+        }
         ?>
-        <div class="col-4 text-align">
-            <h3>5</h3>
-            Categories
-        </div>
-        <div class="col-4 text-align">
-            <h3>5</h3>
-            Categories
-        </div>
-        <div class="col-4 text-align">
-            <h3>5</h3>
-            Categories
-        </div>
-        <div class="col-4 text-align">
-            <h3>5</h3>
-            Categories
-        </div>
+
+        <!-- calculating the total number of categories -->
+        <?php
+
+        //query to calculate the number of categories
+        $sql = "SELECT * FROM tbl_category";
+        $res = mysqli_query($conn, $sql);
+        if ($res) {
+            $count = mysqli_num_rows($res);
+            ?>
+            <div class="col-4 text-align">
+                <h3>
+                    <?php echo $count ?>
+                </h3>
+                Categories
+            </div>
+            <?php
+        }
+        ?>
+
+        <!-- calculating the total number of food item -->
+        <?php
+        $sql1 = "SELECT * FROM tbl_food";
+        $res1 = mysqli_query($conn, $sql1);
+        if ($res) {
+            $count1 = mysqli_num_rows($res);
+            ?>
+            <div class="col-4 text-align">
+                <h3>
+                    <?php echo $count ?>
+                </h3>
+                Foods
+            </div>
+            <?php
+        }
+        ?>
+
+        <!-- calculating the total number of order placed -->
+        <?php
+        $sql2 = "SELECT * FROM tbl_order";
+        $res2 = mysqli_query($conn, $sql2);
+        if ($res2) {
+            $count2 = mysqli_num_rows($res2);
+            ?>
+            <div class="col-4 text-align">
+                <h3>
+                    <?php echo $count2 ?>
+                </h3>
+                Total Orders
+            </div>
+            <?php
+        }
+        ?>
+
+        <!-- calculating the total number of order placed -->
+        <?php
+        $sql3 = "SELECT sum(total) AS Total FROM tbl_order";
+        $res3 = mysqli_query($conn, $sql3);
+        if ($res3) {
+            $row = mysqli_fetch_assoc($res3);
+
+            $total_revenue = $row['Total'];
+            ?>
+            <div class="col-4 text-align">
+                <h3><?php echo $total_revenue?></h3>
+                Revenue Generated
+            </div>
+            <?php
+        }
+        ?>
+
+
+
+
 
         <div class="clearfix"></div>
     </div>
